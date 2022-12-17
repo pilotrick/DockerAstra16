d67b48d0054ffe5fa0ddac5401b00a8a49a39e1b
Ricardo Rodriguez <ricky@ippdr.com>
2022-12-17 17:55:10 -0400

    aaaaa

--- addons/16/account_line_view/models/account_move_line.py
+++ addons/16/account_line_view/models/account_move_line.py
@@ -1,25 +1,4 @@
 # -*- coding: utf-8 -*-
-#############################################################################
-#
-#    Cybrosys Technologies Pvt. Ltd.
-#
-#    Copyright (C) 2022-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
-#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
-#
-#    You can modify it under the terms of the GNU LESSER
-#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
-#
-#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
-#    (LGPL v3) along with this program.
-#    If not, see <http://www.gnu.org/licenses/>.
-#
-#############################################################################
-
 
 from odoo import models, fields
 
@@ -28,3 +7,19 @@ class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
     product_image = fields.Binary(related='product_id.image_1920')
+    warehouse = fields.Many2one(comodel_name='stock.warehouse',string="Warehouse",related='move_id.warehouse_id')
+    seller = fields.Many2one(comodel_name='res.users',string='Vendedor',related='move_id.invoice_user_id', readonly=True)
+    margin_amount = fields.Char(string='margen',related='move_id.margin_amount')
+    margin_percentage = fields.Char(String="Margen(%)", related='move_id.margin_percentage')
+    partner_id = fields.Many2one(comodel_name='res.partner', string='Clinte')
+    origin_document = fields.Char(string='Documento de Origen', related='move_id.invoice_origin')
+    cost = fields.Float (string="Costo", related='product_id.standard_price')
+    product_category = fields.Many2one (string="Categoria de producto", related='product_id.categ_id')
+    payment_status = fields.Selection([('not_paid','No pagadas'),('in_payment','En proceso de pago'),('paid','Pagado'),('partial','Pagado Parcialmente'),('reversed','Revertido'),('invoicing_legacy','Factura Sistema Anterior')], string="Estado de pago", related='move_id.payment_state')
+    amount_total_signed = fields.Monetary(string="Total", related='move_id.amount_total_signed')
+    
+    
+    company_id = fields.Many2one('res.company', default=lambda self: self.env.user.company_id, store=True)
+    currency_id = fields.Many2one('res.currency', related='company_id.currency_id', readonly=True, store=True)
+    
+    
